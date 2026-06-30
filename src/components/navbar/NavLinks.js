5649adf4e656da9bc4ee4ccdb8b381bc0a8ca953
Sato Haruto <satoharuto961@gmail.com>
2026-06-30 11:38:04 +0900

    update contact info

--- src/components/navbar/NavLinks.js
+++ src/components/navbar/NavLinks.js
@@ -7,8 +7,8 @@ const NavLinks = ({handleNav}) => {
   return (
     <ul className='nav-links'>
         <li onClick={handleNav} ><a href="//github.com/whoisdjam/" target='_blank' rel="noopener noreferrer" className='nav-link'><BsGithub /></a></li>
-        <li onClick={handleNav} ><a href="//wa.me/+819094373585" target='_blank' rel="noopener noreferrer" className='nav-link'><BsWhatsapp /></a></li>
-        <li onClick={handleNav} ><a href="//t.me/swilson7565" target='_blank' rel="noopener noreferrer" className='nav-link'><BsTelegram /></a></li>
+        {/* <li onClick={handleNav} ><a href="//wa.me/+819094373585" target='_blank' rel="noopener noreferrer" className='nav-link'><BsWhatsapp /></a></li> */}
+        <li onClick={handleNav} ><a href="//t.me/@binary816" target='_blank' rel="noopener noreferrer" className='nav-link'><BsTelegram /></a></li>
         <li onClick={handleNav} ><a href="mailto:satoharuto961@gmail.com" className='nav-link' rel="noopener noreferrer"><BiEnvelope /></a></li>
   </ul>
   )
